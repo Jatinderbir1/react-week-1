7b308aa5d75152db821d385bb84f4b27ab1ef989
Jatinderbir1 <119245792+Jatinderbir1@users.noreply.github.com>
2023-04-18 10:19:03 +0530

    Add files via upload

---
+++ src/App.tsx
@@ -0,0 +1,103 @@
+import { useState } from 'react';
+
+function App() {
+  const [item, setItem] = useState('');
+  const [quantity, setQuantity] = useState('');
+  const [bucketList, setBucketList] = useState<any>([]);
+
+  const addItemHandler = (e: any) => {
+    e.preventDefault();
+    let addedItem = {
+      id: (Math.random() * 1000).toFixed(0),
+      itemName: item,
+      quantity: quantity,
+    };
+    let currentItemList = bucketList;
+    currentItemList.push(addedItem);
+    setBucketList(currentItemList);
+    setItem('');
+    setQuantity('');
+  };
+
+  const removeListItem = (id: any) => {
+    let newBucketList = bucketList.filter((item: any) => item.id !== id);
+    setBucketList(newBucketList);
+  };
+
+
+  const handleDecrement = (id:any) => {
+    let newBucketList = bucketList.map((item: any) =>{
+      if (item.id === id && item.quantity >= 1) {
+        let newCount = item.quantity;
+        newCount--;
+        return{
+          ...item,
+          quantity: newCount,
+        };
+      } else {
+        return item;
+      }
+    } );
+    setBucketList(newBucketList);
+  };
+
+  const handleIncrement = (id:any) => {
+    let newBucketList = bucketList.map((item: any) =>{
+      if (item.id === id) {
+        let newCount = item.quantity;
+        newCount++;
+        return{
+          ...item,
+          quantity: newCount,
+        };
+      } else {
+        return item;
+      }
+    } );
+    setBucketList(newBucketList);
+  };
+
+
+
+  return (
+    <div className="App">
+      <section className="store">
+        <section>
+          <h1>Store</h1>
+        </section>
+        <form onSubmit={addItemHandler}>
+          <label htmlFor="item">Item</label>
+          <br />
+          <input type="text" id="item" name="item" onChange={(e) => setItem(e.target.value)} value={item}></input>{' '}
+          <br />
+          <label htmlFor="quantity">Quantity</label>
+          <br />
+          <input
+            type="number"
+            id="quantity"
+            name="quantity"
+            onChange={(e) => setQuantity(e.target.value)}
+            value={quantity}
+          ></input>{' '}
+          <br />
+          <input className="button" type="submit" value="Add Item"></input>
+        </form>
+      </section>
+      <section>
+        <section className="store">
+          <h1>Bucket</h1>
+        </section>
+        {bucketList.map((item: any) => (
+          <div   key={item.id} 
+          className="listItem">
+            {item.itemName} x {item.quantity}
+            <button type='button' onClick={() => handleIncrement(item.id)}>+</button>
+            <button type='button' onClick={() => handleDecrement(item.id)}>-</button>
+          </div>
+        ))}
+      </section>
+    </div>
+  );
+}
+
+export default App;
